50d12e9710b7ebfff67f327327006bed43ca7d02
sam-bessey <samueljbessey@gmail.com>
2026-01-04 12:38:12 -0500

    Add working save and delete buttons, and style day/night dropdown Delete button redirects to home page save button saves drive, but doesn't do anything else for now Home page now gets minutes from going through the saved drives and adding up the amount of time instead of needing another thing in local storage Dropdown for day and night now looks better

--- index.js
+++ index.js
@@ -14,20 +14,30 @@ function getData() {
         // TODO: Show login page here
 
         // For now just reset all local storage. Once login page is finished, this will not be here
-        localStorage.setItem("minutes", 0)
-        localStorage.setItem("minutesNight", 0)
         localStorage.setItem("drives", "[]")
         localStorage.setItem("usedBefore", "yes")
     } else {
-        let minutes = localStorage.getItem("minutes"); // Total number of minutes driving, including night
-        let nightMinutes = localStorage.getItem("minutesNight"); // Number of minutes at night
+        // Format of drives:
+        // [number of minutes, date and time, night true/false]
 
         let drives = JSON.parse(localStorage.getItem("drives")) || []; // Get list of all drives
         console.log("drives:", drives);
 
+        // Find total minutes and night minutes from this list of all drives
+        let minutes = 0;
+        let nightMinutes = 0;
+        for (let i = 0; i < drives.length; i++) {
+            minutes += drives[i][0];
+
+            // If drive was at night, also add to the night minutes
+            if (drives[i][2]) {
+                nightMinutes += drives[i][0]
+            }
+        }
+
         // Update display
         document.getElementById("totalHours").innerText = `${divideWithRemainder(minutes, 60)[0]}hr ${divideWithRemainder(minutes, 60)[1]}min`
-        document.getElementById("nightHours").innerText = `${divideWithRemainder(nightMinutes, 60)[0]}hr ${divideWithRemainder(nightMinutes, 60)[1]}min`
+        document.getElementById("nightHours").innerText = `${divideWithRemainder(nightMinutes, 60)[0]}hr ${divideWithRemainder(nightMinutes, 60)[1]}min night`
     }
 }
 
