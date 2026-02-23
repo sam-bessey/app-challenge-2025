7b02bd108cccd3d16e126f2dc33d49f3ecc58044
sam-bessey <samueljbessey@gmail.com>
2026-02-23 16:11:55 -0500

    Add blue theme

--- settings.js
+++ settings.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // Use dark mode if needed
     updateTheme();
 
+    // Update theme menu to show current theme
+    document.getElementById("theme").value = localStorage.getItem("theme") || "Pink";
+
     document.getElementById("theme").addEventListener("change", () => {
         // Update local storage
         localStorage.setItem("theme", document.getElementById("theme").value);
