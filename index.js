1454c936a308d7d247e0d08ee0de7b49d52c8743
Samuel Bessey <samueljbessey@gmail.com>
2026-03-30 21:06:37 -0400

    Make it actually work

--- index.js
+++ index.js
@@ -8,60 +8,69 @@ function updateHomepage() {
         `${divideWithRemainder(getData()[1], 60)[0]}hr ${divideWithRemainder(getData()[1], 60)[1]}min`;
     document.getElementById("nightHours").innerText =
         `${divideWithRemainder(getData()[2], 60)[0]}hr ${divideWithRemainder(getData()[2], 60)[1]}min night`;
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-    // Decide whether or not to use dark mode
-    updateTheme();
+    // Update progress bar
+    const progressBar = document.getElementById("totalProgress");
+    let progress = 0;
+    let endValue = (getData()[1] / 4200) * 100;
 
-    // Get data after page loads
-    updateHomepage();
+    const nightProgressBar = document.getElementById("nightProgress");
+    let nightProgress = 0;
+    let nightEndValue = (getData()[2] / 600) * 100;
 
-    // Hide loading screen after a second
-    setTimeout(() => {
-        document.getElementById("loading").style.opacity = 0;
+    const progressInterval = setInterval(() => {
+        progress += 0.25;
 
-        // Hide loading screen after animation is finished
-        setTimeout(() => {
-            document.getElementById("loading").hidden = true;
-        }, 300);
-
-        // Update progress bar
-        const progressBar = document.getElementById("totalProgress");
-        let progress = 0;
-        let endValue = (getData()[1] / 4200) * 100;
-
-        const nightProgressBar = document.getElementById("nightProgress");
-        let nightProgress = 0;
-        let nightEndValue = (getData()[2] / 600) * 100;
-
-        const progressInterval = setInterval(() => {
-            progress += 0.25;
-
-            // Update the gradient
-            progressBar.style.background = `conic-gradient(
+        // Update the gradient
+        progressBar.style.background = `conic-gradient(
         var(--totalProgressColor) ${progress * 3.6}deg, 
         var(--backgroundColor) ${progress * 3.6}deg
           )`;
 
-            // Check if animation can finish
-            if (progress >= endValue) {
-                clearInterval(progressInterval);
-            }
-        }, 5);
-        const nightProgressInterval = setInterval(() => {
-            nightProgress += 0.15;
+        // Check if animation can finish
+        if (progress >= endValue) {
+            clearInterval(progressInterval);
+        }
+    }, 5);
+    const nightProgressInterval = setInterval(() => {
+        nightProgress += 0.15;
 
-            // Update the gradient
-            nightProgressBar.style.background = `conic-gradient(
+        // Update the gradient
+        nightProgressBar.style.background = `conic-gradient(
             var(--nightProgressColor) ${nightProgress * 3.6}deg, 
             var(--backgroundColor) ${nightProgress * 3.6}deg
             )`;
 
-            // Check if animation can finish
-            if (nightProgress >= nightEndValue) {
-                clearInterval(nightProgressInterval);
-            }
-        }, 5);
-    }, 1000);
+        // Check if animation can finish
+        if (nightProgress >= nightEndValue) {
+            clearInterval(nightProgressInterval);
+        }
+    }, 5);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Show loading screen if needed. Use dark mode session storage to tell
+    if (sessionStorage.getItem("darkMode") === null) {
+        // Show loading screen
+        document.getElementById("loading").hidden = false;
+
+        // Hide loading screen after a second
+        setTimeout(() => {
+            document.getElementById("loading").style.opacity = 0;
+
+            // Update homepage as loading screen is fading
+            updateHomepage();
+
+            // Hide loading screen after animation is finished
+            setTimeout(() => {
+                document.getElementById("loading").hidden = true;
+            }, 300);
+        }, 1000);
+    } else {
+        // Just update homepage without loading screen
+        updateHomepage();
+    }
+
+    // Decide whether or not to use dark mode
+    updateTheme();
 });
