3a40e35e26c1e32f0f0b59f7624b232537579cfd
Samuel Bessey <samueljbessey@gmail.com>
2026-03-30 20:23:41 -0400

    Add loading page with app name Loading page is just for decoration :)

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import {divideWithRemainder, getData, updateTheme} from './functions.js';
+import { divideWithRemainder, getData, updateTheme } from "./functions.js";
 
 function updateHomepage() {
     // Gets saved data and updates homepage with it
 
     // Update display
-    document.getElementById("totalHours").innerText = `${divideWithRemainder(getData()[1], 60)[0]}hr ${divideWithRemainder(getData()[1], 60)[1]}min`;
-    document.getElementById("nightHours").innerText = `${divideWithRemainder(getData()[2], 60)[0]}hr ${divideWithRemainder(getData()[2], 60)[1]}min night`;
-
+    document.getElementById("totalHours").innerText =
+        `${divideWithRemainder(getData()[1], 60)[0]}hr ${divideWithRemainder(getData()[1], 60)[1]}min`;
+    document.getElementById("nightHours").innerText =
+        `${divideWithRemainder(getData()[2], 60)[0]}hr ${divideWithRemainder(getData()[2], 60)[1]}min night`;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -16,41 +17,51 @@ document.addEventListener("DOMContentLoaded", () => {
     // Get data after page loads
     updateHomepage();
 
-    // Update progress bar
-    const progressBar = document.getElementById("totalProgress");
-    let progress = 0;
-    let endValue = getData()[1] / 4200 * 100;
+    // Hide loading screen after a second
+    setTimeout(() => {
+        document.getElementById("loading").style.opacity = 0;
 
-    const nightProgressBar = document.getElementById("nightProgress");
-    let nightProgress = 0;
-    let nightEndValue = getData()[2] / 600 * 100;
+        // Hide loading screen after animation is finished
+        setTimeout(() => {
+            document.getElementById("loading").hidden = true;
+        }, 300);
 
-    const progressInterval = setInterval(() => {
-        progress += 0.25;
+        // Update progress bar
+        const progressBar = document.getElementById("totalProgress");
+        let progress = 0;
+        let endValue = (getData()[1] / 4200) * 100;
 
-        // Update the gradient
-        progressBar.style.background = `conic-gradient(
+        const nightProgressBar = document.getElementById("nightProgress");
+        let nightProgress = 0;
+        let nightEndValue = (getData()[2] / 600) * 100;
+
+        const progressInterval = setInterval(() => {
+            progress += 0.25;
+
+            // Update the gradient
+            progressBar.style.background = `conic-gradient(
         var(--totalProgressColor) ${progress * 3.6}deg, 
         var(--backgroundColor) ${progress * 3.6}deg
           )`;
 
-        // Check if animation can finish
-        if (progress >= endValue) {
-            clearInterval(progressInterval);
-        }
-    }, 5);
-    const nightProgressInterval = setInterval(() => {
-        nightProgress += 0.15;
+            // Check if animation can finish
+            if (progress >= endValue) {
+                clearInterval(progressInterval);
+            }
+        }, 5);
+        const nightProgressInterval = setInterval(() => {
+            nightProgress += 0.15;
 
-        // Update the gradient
-        nightProgressBar.style.background = `conic-gradient(
+            // Update the gradient
+            nightProgressBar.style.background = `conic-gradient(
             var(--nightProgressColor) ${nightProgress * 3.6}deg, 
             var(--backgroundColor) ${nightProgress * 3.6}deg
             )`;
 
-        // Check if animation can finish
-        if (nightProgress >= nightEndValue) {
-            clearInterval(nightProgressInterval);
-        }
-    }, 5);
+            // Check if animation can finish
+            if (nightProgress >= nightEndValue) {
+                clearInterval(nightProgressInterval);
+            }
+        }, 5);
+    }, 1000);
 });
